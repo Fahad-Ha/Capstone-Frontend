8adbeaf9cd53c92d48c9483a0706b45f70b4eea7
Fahad <fahhod.ahmad@gmail.com>
2023-08-08 18:23:11 +0300

    fixed issue in users page and added profile page

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { getToken } from "./auth/storage";
 
-const BASE_URL = "http://192.168.8.177:8080";
+const BASE_URL = "http://192.168.2.193:8080";
 // const BASE_URL = "http://192.168.8.192:8000";
 // const BASE_URL = "http://localhost:8000";
 
